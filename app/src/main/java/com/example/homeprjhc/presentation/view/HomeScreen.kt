2fb2a448451c2c1c082bb0e22c2852c93a52d86f
baronn <baronn.r99@gmail.com>
2024-12-27 23:20:09 -0600

    optimize imports

--- app/src/main/java/com/example/homeprjhc/presentation/view/HomeScreen.kt
+++ app/src/main/java/com/example/homeprjhc/presentation/view/HomeScreen.kt
@@ -11,8 +11,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
--- app/src/main/java/com/example/homeprjhc/presentation/view/WeatherCity.kt
+++ app/src/main/java/com/example/homeprjhc/presentation/view/WeatherCity.kt
@@ -1,17 +1,11 @@
 package com.example.homeprjhc.presentation.view
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
